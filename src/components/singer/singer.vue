b5920b3adc08de7c6480f3680b1effb40cb3794a
周岐同 <zhou_qt@foxmail.com>
2017-07-19 20:30:53 +0800

    first

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="singer">
+    歌手
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
